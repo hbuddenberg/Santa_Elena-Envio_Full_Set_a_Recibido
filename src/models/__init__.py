1a3a02c86e4788fa62165ee9d994bc7fa6952390
HansBuddenberg-SmartBots <hans.buddenberg@smart-bots.cl>
2025-02-25 00:38:13 -0300

    Agregar módulo de estructuración de registros, implementar clase CasoExportacion y eliminar archivos obsoletos

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from .replace_placeholders import replace_placeholders
 from .config_yaml import load_config as load_config_yaml
 from .config_excel import load_config as load_config_excel
+from .caso_exportacion import CasoExportacion
 
-__all__ = ['replace_placeholders', 'load_config_yaml', 'load_config_excel']
+__all__ = ['replace_placeholders', 'load_config_yaml', 'load_config_excel', 'CasoExportacion']
