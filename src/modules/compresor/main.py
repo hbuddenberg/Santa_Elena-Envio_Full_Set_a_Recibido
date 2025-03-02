cfa1b0d7bb65d1970e6bad1ee99097e6103bb991
Hans-Dieter Buddenberg Blamey <hans.buddenberg@smart-bots.cl>
2025-03-02 18:07:49 -0300

    Eliminar archivos obsoletos y actualizar plantillas de configuración

--- src/modules/compresor/main.py
+++ src/modules/compresor/main.py
@@ -1,5 +1,5 @@
 import os
-import zipfile
+import py7zr
 
 def obtener_tamaño_total(archivos):
     tamaño_total = 0
@@ -7,26 +7,28 @@ def obtener_tamaño_total(archivos):
         tamaño_total += os.path.getsize(archivo) / (1024 * 1024)  # Convertir a MB
     return tamaño_total
 
-def comprimir_archivos(archivos, archivo_zip, ruta_base):
-    ruta_completa_zip = os.path.join(ruta_base, archivo_zip)
-    with zipfile.ZipFile(ruta_completa_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+def comprimir_archivos(archivos, archivo_7z, ruta_base):
+    ruta_completa_7z = os.path.join(ruta_base, archivo_7z)
+    with py7zr.SevenZipFile(ruta_completa_7z, 'w') as archive:
         for archivo in archivos:
             ruta_completa = os.path.join(ruta_base, archivo)
-            zipf.write(ruta_completa, os.path.basename(ruta_completa))
-    return archivo_zip
+            archive.write(ruta_completa, os.path.basename(ruta_completa))
+    return archivo_7z
 
 def validar_archivos(archivos, ruta_base, nombre_comprimido, tamaño_maximo=25):
     tamaño_total = obtener_tamaño_total([os.path.join(ruta_base, archivo) for archivo in archivos])
     if tamaño_total > tamaño_maximo:
-        archivo_zip = f'{nombre_comprimido}.zip'
-        return [comprimir_archivos(archivos, archivo_zip, ruta_base)], tamaño_total
+        archivo_7z = f'{nombre_comprimido}.7z'
+        archivo_comprimido = [comprimir_archivos(archivos, archivo_7z, ruta_base)]
+        tamaño_comprimido = obtener_tamaño_total([os.path.join(ruta_base, archivo) for archivo in archivo_comprimido])
+        return archivo_comprimido, tamaño_comprimido
     else:
         return archivos, tamaño_total
 
 def main():
     asunto = 'FULL SET OF DOCS OE232400596 -OE232400597 -OE232400598 -OE232400599- MAERSK BULAN - TROPME (ETA 01-05-2024)'
-    ruta_base = '/Volumes/Resources/Development/SmartBots/Santa_Elena-Envio_Full_Set_a_Recibido/test/En Proceso/FULL SET OF DOCS OE232400596 -OE232400597 -OE232400598 -OE232400599- MAERSK BULAN - TROPME (ETA 01-05-2024)'  # Ajusta esta ruta según tus necesidades
-    archivos = ['FULL SET OE232400596- MAERSK BULAN - TROPME-1.pdf', 'FULL SET OE232400596- MAERSK BULAN - TROPME.pdf']
+    ruta_base = 'D:/Dev/Santa_Elena-Envio_Full_Set_a_Recibido/test/En Proceso/FULL SET OF DOCS OE232400596 -OE232400597 -OE232400598 -OE232400599- MAERSK BULAN - TROPME (ETA 01-05-2024)'
+    archivos = ['FULL SET OE232400596- MAERSK BULAN - TROPME-1.pdf ', 'FULL SET OE232400596- MAERSK BULAN - TROPME.pdf ', 'FULL SET OE232400597- MAERSK BULAN - TROPME.pdf ', 'PACKING LIST  - OE232400597_MAERSK BULAN_TROPME.xls ', 'PACKING LIST - OE232400598_MAERSK BULAN_TROPME.xls ', 'PACKING LIST - OE232400599_MAERSK BULAN_TROPME.xls']
     archivos_validados, tamaño_total = validar_archivos(archivos, ruta_base, asunto)
     print(f"Archivos a enviar: {archivos_validados}")
 
